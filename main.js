f3d2fc8934c9eebc7d5a19f39d53086e72cb6cbd
angelr1076 <angelr1076@gmail.com>
2023-02-24 06:37:37 -0500

    Added fib iteration and recursive functions

---
+++ main.js
@@ -0,0 +1,42 @@
+// Using iteration, write a function fibs which takes a number and returns an array containing that many numbers from the fibonacci sequence. Using an example input of 8, this method should return the array [0, 1, 1, 2, 3, 5, 8, 13].
+function fibs(n) {
+  let fibArray = [];
+
+  debugger;
+
+  for (let i = 0; i < n; i++) {
+    if (i === 0) {
+      fibArray.push(0);
+    } else if (i === 1) {
+      fibArray.push(1);
+    } else {
+      let lastFib = fibArray[fibArray.length - 1];
+      let secondLastFib = fibArray[fibArray.length - 2];
+      fibArray.push(lastFib + secondLastFib);
+    }
+  }
+
+  return fibArray;
+}
+
+// let fibsEight = fibs(8);
+// console.log(fibsEight);
+
+// Now write another method fibsRec which solves the same problem recursively. This can be done in just a couple of lines (or 1 if you’re crazy, but don’t consider either of these lengths a requirement… just get it done).
+// @ts-check
+function fibRecursive(n) {
+  debugger;
+  if (n <= 0) {
+    return [];
+  } else if (n === 1) {
+    return [0];
+  } else if (n === 2) {
+    return [0, 1];
+  } else {
+    var prev = fibRecursive(n - 1);
+    return prev.concat(prev[prev.length - 1] + prev[prev.length - 2]);
+  }
+}
+
+let fibEight = fibRecursive(8);
+console.log(fibEight);
